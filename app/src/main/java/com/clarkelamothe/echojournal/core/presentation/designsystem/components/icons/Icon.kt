4e8b8371f334bd3bca22c79f0ce2beef30612187
Clarke Lamothe <clarkelamothe@yahoo.fr>
2025-01-06 18:09:28 -0300

    added more icons

---
+++ app/src/main/java/com/clarkelamothe/echojournal/core/presentation/designsystem/components/icons/Icon.kt
@@ -0,0 +1,50 @@
+package com.clarkelamothe.echojournal.core.presentation.designsystem.components.icons
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.clarkelamothe.echojournal.R
+
+val SettingsIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_settings)
+
+val EditIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_edit)
+
+val AddIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_add)
+
+val CheckIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_check)
+
+val CloseIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_close)
+
+val BackIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_back)
+
+val HashtagIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_hashtag)
+
+val PlayIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_play)
+
+val PauseIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_pause)
+
+val MicIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_mic)
+
+val AiIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_ai)
