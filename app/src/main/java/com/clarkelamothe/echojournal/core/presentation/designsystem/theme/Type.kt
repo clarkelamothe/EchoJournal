68fe83b7cf1ce854763887c8b10f97605c06a919
Clarke Lamothe <clarkelamothe@yahoo.fr>
2025-01-06 21:05:09 -0300

    added inter font

--- app/src/main/java/com/clarkelamothe/echojournal/core/presentation/designsystem/theme/Type.kt
+++ app/src/main/java/com/clarkelamothe/echojournal/core/presentation/designsystem/theme/Type.kt
@@ -2,33 +2,65 @@ package com.clarkelamothe.echojournal.core.presentation.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontLoadingStrategy
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.clarkelamothe.echojournal.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+private val inter = FontFamily(
+    Font(
+        R.font.inter,
+        weight = FontWeight.Normal,
+        loadingStrategy = FontLoadingStrategy.Async
+    ),
+    Font(
+        R.font.inter_medium,
+        weight = FontWeight.Medium,
+        loadingStrategy = FontLoadingStrategy.Async
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+)
+
+val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 26.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = inter,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = inter,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp
     )
-    */
 )
